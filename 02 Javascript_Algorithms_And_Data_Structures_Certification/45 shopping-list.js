32a6bff4aa132d14ebfa90921fe6f6e5e484e6a0
Pocholo Tan <poch.tan@yahoo.com>
2018-09-03 19:25:03 -0400

    First commit

---
+++ 02 Javascript_Algorithms_And_Data_Structures_Certification/45 shopping-list.js
@@ -0,0 +1,7 @@
+var myList = [
+              ["Lindt Lindor", 5],
+              ["DQ Blizzard", 4],
+              ["Quaker Dipps", 3],
+              ["Stuffed Oreo", 2],
+              ["Twix", 1]
+             ];
